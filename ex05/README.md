1e283037208910df7dd38fb92f22b8e05ac406d0
Erik Ap Gomes Rocha <erik.gomes35@gmail.com>
2026-02-16 09:22:24 -0300

    Adicionado o README e o main.py do exercicio 5

---
+++ ex05/README.md
@@ -0,0 +1,6 @@
+
+
+# Exercícios 5 - Antecessor e Sucessor
+
+Este exercício mostra o número antes e depois de um número dado.
+
---
+++ ex05/main.py
@@ -0,0 +1,2 @@
+num = int(input("Digite um número: "))
+print("O número dado foi {num}, seu antecessor é {num-1} e seu sucessor é {num+1}")
