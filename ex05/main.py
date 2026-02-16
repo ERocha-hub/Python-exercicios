bb58d49baf5326c7ebbe6859d60a63bb5faa9987
Erik Ap Gomes Rocha <erik.gomes35@gmail.com>
2026-02-16 09:26:30 -0300

    Corrigido: Esqueci de fazer a string formatada

--- ex05/main.py
+++ ex05/main.py
@@ -1,2 +1,2 @@
 num = int(input("Digite um número: "))
-print("O número dado foi {num}, seu antecessor é {num-1} e seu sucessor é {num+1}")
+print(f"O número dado foi {num}, seu antecessor é {num-1} e seu sucessor é {num+1}")
